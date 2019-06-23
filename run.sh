56924b3dc2f82e741548a7cdad1af8af0e83dd9e
linyz <linyz@pku.edu.cn>
2019-06-23 11:08:06 +0800

    推送代码到github

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ python run_cmrc2019_baseline.py \
 	--train_file cmrc2019_train.json \
 	--predict_file cmrc2019_trial.json \
 	--train_batch_size 24 \
-	--learning_rate 2e-5 \
+	--learning_rate 2e-5 \git
 	--num_train_epochs 3.0 \
 	--max_seq_length 512 \
 	--output_dir ./output_model
